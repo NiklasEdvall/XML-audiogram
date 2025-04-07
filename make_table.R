ba9f71278bfcd5b812248ab754d2e556c465d0d0
NiklasEdvall <niklasedvall@gmail.com>
2025-04-07 15:57:16 +0200

    table1

--- make_table.R
+++ make_table.R
@@ -1,1 +1,24 @@
-# Table goes here
+
+library(readxl)
+library(arsenal)
+
+# Read excel sheet
+dat <- read_excel("data/Exempel_FFdata.xlsx", sheet = 1)
+
+# Specify factor variables and categoires
+dat$Sex <- factor(dat$Sex, levels = c(1,2,3,4), labels = c("M", "F", "Other", "No response"))
+dat$Hearing <- factor(dat$Hearing, levels = c(1,2,3), labels = c("No problem", "Sometimes", "General problem"))
+dat$Tinnitus <- factor(dat$Tinnitus, levels = c(1,2,3), labels = c("No", "Yes, Sometimes", "Yes, Always"))
+dat$Noise <- factor(dat$Noise, levels = c(1,2,3), labels = c("Low", "Medium", "High"))
+
+# Make table
+tab1 <- tableby(data = dat, Sex ~ Age + Hearing + Tinnitus + Noise,
+                test = FALSE, # Include statistical test?
+                total = FALSE, # Include column for total?
+                digits = 1) # Digits to round to
+
+# Print table
+summary(tab1, text = TRUE)
+
+# Save table in word document
+write2word(tab1, "data/table1.docx")
